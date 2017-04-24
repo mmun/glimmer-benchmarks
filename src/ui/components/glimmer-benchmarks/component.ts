660a7b2a16b99fbc570db851d6ec31db1e0acd68
Martin Muñoz <im.mmun@gmail.com>
2017-04-24 16:41:31 -0400

    Implement some benchmarks to test autotracked

--- src/ui/components/glimmer-benchmarks/component.ts
+++ src/ui/components/glimmer-benchmarks/component.ts
@@ -1,5 +1,55 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
+
+const DEPTH = 10;
+
+class Node {
+  @tracked left: Node;
+  @tracked rite: Node;
+
+  constructor(depth) {
+    this.left = depth > 0 ? new Node(depth-1) : null;
+    this.rite = depth > 0 ? new Node(depth-1) : null;
+  }
+
+  @tracked get count() {
+    let leftCount = (this.left == null) ? 0 : this.left.count;
+    let riteCount = (this.rite == null) ? 0 : this.rite.count;
+    return leftCount + riteCount + 1;
+  }
+
+}
+
+let initNodesStartTime;
+let initNodesEndTime;
+let didInsertElementTime;
 
 export default class GlimmerBenchmarks extends Component {
+  @tracked root
 
+  constructor(options) {
+    super(options);
+
+    initNodesStartTime = performance.now();
+    let root = new Node(DEPTH);
+    initNodesEndTime = performance.now();
+
+    this.root = root;
+  }
+
+  didInsertElement() {
+    didInsertElementTime = performance.now();
+
+    document.write(`
+      <table border=1>
+        <tr>
+          <td>time to allocate nodes</td>
+          <td style="text-align: right">${Math.round(initNodesEndTime - initNodesStartTime)} ms</td>
+        </tr>
+        <tr>
+          <td>time to insert element</td>
+          <td style="text-align: right">${Math.round(didInsertElementTime - initNodesEndTime)} ms</td>
+        </tr>
+      </table>
+     `);
+  }
 }
